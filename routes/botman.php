061385f92b69cbabd723f015bc826c055609e410
Milad Kardgar <mk.kardgar@gmail.com>
2019-08-29 16:22:09 +0430

    create project

--- routes/botman.php
+++ routes/botman.php
@@ -6,21 +6,19 @@ use App\Conversations\ExampleConversation;
 
 $botman = resolve('botman');
 
-
 $botman->hears('/start', function ($bot) {
     $user = $bot->getUser();
     $bot->reply('Hello ' . $user->getFirstName() . ' ' . $user->getLastName());
     $bot->reply('Your username is: ' . $user->getUsername());
     $bot->reply('Your ID is: ' . $user->getId());
-
-    \App\User::create(
-        [
-            'chat_id' => $user->getId(),
-            'name' => $user->getFirstName(),
-            'last_name' => $user->getLastName(),
-            'username' => $user->getUsername(),
-        ]
-    );
+//    \App\User::create(
+//        [
+//            'chat_id' => $user->getId(),
+//            'name' => $user->getFirstName(),
+//            'last_name' => $user->getLastName(),
+//            'username' => $user->getUsername(),
+//        ]
+//    );
 });
 $botman->hears('/setting', ManageController::class . '@setting');
 
