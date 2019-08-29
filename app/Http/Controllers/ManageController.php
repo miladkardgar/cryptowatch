e85eb53b209802fb47fcf1738b290c849322a8d3
Milad Kardgar <mk.kardgar@gmail.com>
2019-08-29 16:26:15 +0430

    create project

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+
 class ManageController extends Controller
 {
     //
     public function start($bot)
     {
+
+        $user = $bot->getUser();
+        $bot->reply('Hello ' . $user->getFirstName() . ' ' . $user->getLastName());
+        $bot->reply('Your username is: ' . $user->getUsername());
+        $bot->reply('Your ID is: ' . $user->getId());
+        User::create(
+            [
+                'chat_id' => $user->getId(),
+                'name' => $user->getFirstName(),
+                'last_name' => $user->getLastName(),
+                'username' => $user->getUsername(),
+            ]
+        );
     }
 }
--- routes/botman.php
+++ routes/botman.php
@@ -6,20 +6,7 @@ use App\Conversations\ExampleConversation;
 
 $botman = resolve('botman');
 
-$botman->hears('/start', function ($bot) {
-    $user = $bot->getUser();
-    $bot->reply('Hello ' . $user->getFirstName() . ' ' . $user->getLastName());
-    $bot->reply('Your username is: ' . $user->getUsername());
-    $bot->reply('Your ID is: ' . $user->getId());
-    \App\User::create(
-        [
-            'chat_id' => $user->getId(),
-            'name' => $user->getFirstName(),
-            'last_name' => $user->getLastName(),
-            'username' => $user->getUsername(),
-        ]
-    );
-});
+$botman->hears('/start', 'App\Http\Controllers\ManageController@start');
 $botman->hears('/setting', ManageController::class . '@setting');
 
 
