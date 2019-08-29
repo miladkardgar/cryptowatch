63c44156c8f534018fee4175364bfa99176fbf5b
Milad Kardgar <mk.kardgar@gmail.com>
2019-08-29 19:24:28 +0430

    create project

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command(data::class.'@getData()')
+         $schedule->command(data::class.'@getData')
                   ->everyMinute();
     }
 
