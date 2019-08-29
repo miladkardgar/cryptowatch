a5a974f614f0828c4bac90e5f94d48b119db1d03
Milad Kardgar <mk.kardgar@gmail.com>
2019-08-29 16:17:06 +0430

    create project

--- routes/botman.php
+++ routes/botman.php
@@ -9,10 +9,18 @@ $botman = resolve('botman');
 
 $botman->hears('/start', function ($bot) {
     $user = $bot->getUser();
-    $bot->reply('Hello ' . print_r($user));
     $bot->reply('Hello ' . $user->getFirstName() . ' ' . $user->getLastName());
     $bot->reply('Your username is: ' . $user->getUsername());
     $bot->reply('Your ID is: ' . $user->getId());
+
+    \App\User::create(
+        [
+            'chat_id' => $user->getId(),
+            'name' => $user->getFirstName(),
+            'last_name' => $user->getLastName(),
+            'username' => $user->getUsername(),
+        ]
+    );
 });
 $botman->hears('/setting', ManageController::class . '@setting');
 
