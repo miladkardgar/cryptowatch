2f97f721cc71ebfc669c3b79404bfeccc25ce652
Milad Kardgar <mk.kardgar@gmail.com>
2019-08-29 19:40:05 +0430

    create project

--- app/Console/Commands/getData.php
+++ app/Console/Commands/getData.php
@@ -39,6 +39,66 @@ class getData extends Command
     public function handle()
     {
         //
+
+        $botman = app('botman');
+        $res = '';
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "apikey: TZMPp6CmDEBJIgESoINrRmBu963Pft1OZc9iz6lia8xAFcL6RHIrqrLQAKfAGcDL",
+                "cache-control: no-cache",
+                "postman-token: f261e154-5bfd-5091-6ef1-161c14d9bcf2",
+                "secretkey: oSaJwiMYrA331cL6kPOuMK3bLGPNOu723CQlghqLY9oJIQgSP9kPm1dFSDlJKAwn"
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $response = json_decode($response, true);
+            $res .= "---------------------------------\n";
+            $res .= "┌Symble: #" . $response['symbol'] . "\n\n";
+            $res .= "├price: " . $response['price'] . "\n";
+        }
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "apikey: TZMPp6CmDEBJIgESoINrRmBu963Pft1OZc9iz6lia8xAFcL6RHIrqrLQAKfAGcDL",
+                "cache-control: no-cache",
+                "postman-token: f261e154-5bfd-5091-6ef1-161c14d9bcf2",
+                "secretkey: oSaJwiMYrA331cL6kPOuMK3bLGPNOu723CQlghqLY9oJIQgSP9kPm1dFSDlJKAwn"
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $response = json_decode($response, true);
+            $res .= "├AVGPrice: \n";
+            $res .= "┊├minutes: " . $response['mins'] . "\n";
+            $res .= "┊├Price: " . $response['price'] . "\n";
+        }
+
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => "https://api.binance.com/api/v1/ticker/24hr?symbol=BTCUSDT",
@@ -62,17 +122,13 @@ class getData extends Command
             echo "cURL Error #:" . $err;
         } else {
             $response = json_decode($response, true);
-            print_r($response);
-            $botman = app('botman');
             $botman->say("Data List:", env('TELEGRAM_CHANNEL'), TelegramDriver::class);
-            $res = '';
-            $res .= "---------------------------------\n";
-            $res .= "Symble: " . $response['symbol'] . "\n\n";
-            $res .= "Price: " . $response['priceChange'] . "\n";
-            $res .= "Price Percent: " . $response['priceChangePercent'] . "\n";
-            $res .= "Volume: " . $response['volume'] . "\n";
-            $res .= "quoteVolume: " . $response['quoteVolume'] . "\n";
-            $res .= "count: " . $response['count'] . "\n";
+            $res .= "├24hr: \n";
+            $res .= "┊├Price: " . $response['priceChange'] . "\n";
+            $res .= "┊├Price Percent: " . $response['priceChangePercent'] . "\n";
+            $res .= "┊├Volume: " . $response['volume'] . "\n";
+            $res .= "┊├quoteVolume: " . $response['quoteVolume'] . "\n";
+            $res .= "┊├count: " . $response['count'] . "\n";
             $res .= "\n\n @cryptoowatch \n";
             $res .= "---------------------------------\n\n";
             $botman->say($res, env('TELEGRAM_CHANNEL'), TelegramDriver::class);
