9bdd4265d4696c5fde833561d479d8c42522b3f4
Milad Kardgar <mk.kardgar@gmail.com>
2019-08-29 11:10:04 +0430

    create project

--- routes/botman.php
+++ routes/botman.php
@@ -3,6 +3,11 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
+
+
+$botman->hears('start_chat', BotManController::class.'@startConversation');
+
+
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
